af9c377c66efbc2f21acdd2f6d6a1d227754a68b
Maxime Ryheul <maxime.ryheul@student.howest.be>
2022-05-19 16:47:11 +0200

    zonder button full screen

--- script/app.js
+++ script/app.js
@@ -7,22 +7,22 @@ document.addEventListener('DOMContentLoaded', function() {
     var joy = new JoyStick('joyDiv');
     var joy2 = new JoyStick('joyDiv2');
 
-    const btnfull =  document.querySelector(".js-button-full")
-    btnfull.addEventListener("click",function(){
-        let elem = document.documentElement;
-        if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-          } else if (elem.mozRequestFullScreen) { /* Firefox */
-            elem.mozRequestFullScreen();
-          } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-            elem.webkitRequestFullscreen();
-          } else if (elem.msRequestFullscreen) { /* IE/Edge */
-            elem.msRequestFullscreen();
-          }
+    // const btnfull =  document.querySelector(".js-button-full")
+    // btnfull.addEventListener("click",function(){
+    //     let elem = document.documentElement;
+    //     if (elem.requestFullscreen) {
+    //         elem.requestFullscreen();
+    //       } else if (elem.mozRequestFullScreen) { /* Firefox */
+    //         elem.mozRequestFullScreen();
+    //       } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    //         elem.webkitRequestFullscreen();
+    //       } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    //         elem.msRequestFullscreen();
+    //       }
         
-        // document.documentElement.webkitRequestFullScreen();
-        screen.orientation.lock('landscape');
-    })  
+    //     // document.documentElement.webkitRequestFullScreen();
+    //     screen.orientation.lock('landscape');
+    // })  
     
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('./serviceWorker.js')
