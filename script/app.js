9130687824c161140cec72c9467157ae47842a0a
Maxime Ryheul <maxime.ryheul@student.howest.be>
2022-05-19 11:32:08 +0200

    push

--- script/app.js
+++ script/app.js
@@ -1,10 +1,1 @@
-if (document.body.webkitRequestFullScreen) {
-    window.addEventListener('click', function(e) {
-      if (e.target.type != 'text' && e.target.type != 'password') {
-        body.webkitRequestFullScreen();
-        window.setTimeout(function() {
-          document.webkitCancelFullScreen();
-        }, 500);
-      }
-    }, false);
-  }
+element.requeestFullscreen()
