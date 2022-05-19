e7128f58b0b77c22a4b58881ebc4d4cdcb7e706f
Maxime Ryheul <maxime.ryheul@student.howest.be>
2022-05-19 11:38:27 +0200

    button go full screen

--- script/app.js
+++ script/app.js
@@ -1,1 +1,14 @@
-element.requeestFullscreen()
+// element.requeestFullscreen()
+// JavaScript
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // document.documentElement.webkitRequestFullScreen();
+
+    const btnfull =  document.querySelector(".js-button-full")
+    btnfull.addEventListener("click",function(){
+        document.documentElement.webkitRequestFullScreen();
+    })  
+    
+    
+});
