8518c4de521f3af30a620aaea202914dc5d2df0e
Maxime Ryheul <maxime.ryheul@student.howest.be>
2022-05-31 15:17:37 +0200

    aanpassing hoofd

--- Frontend copy/script/app.js
+++ Frontend copy/script/app.js
@@ -14,7 +14,7 @@ var sketchWidth;
 var sketchHeight;
 var size_head
 var movement_down 
-var direction = 1;
+var inverted = false;
 
 function setup() {
   // createCanvas(500, 500, WEBGL);?
@@ -37,7 +37,7 @@ function setup() {
 }
 
 function draw() {
-  if (direction == 1) {
+  if (inverted == false) {
     pointLight(200,200,200,20,-250,300);   
     // pointLight(100,0,255,10,-50,100);  // kleur thanos 
     background('#fff');
@@ -58,6 +58,27 @@ function draw() {
     // normalMaterial(); // For effect
     model(head);
   }  
+  if(inverted == true){
+    background('#fff');
+    pointLight(200,200,200,20,-250,300);   
+    // pointLight(100,0,255,10,-50,100);  // kleur thanos 
+    rectMode(CENTER);
+    translate(0,movement_down);
+    scale(size_head); // Scaled to make model fit into canvas
+    // scale(20); // Scaled to make thanos fit into canvas
+    rotateX(1.40);
+    rotateX(position_x)
+    rotateY(position_y);
+    rotateZ(- position_y)    
+    // rotateZ(3)
+    rotateZ(3.15) // rotation thanos
+    
+    noStroke();
+    ambientMaterial(255,255,255); 
+    // ambientLight(100);
+    // normalMaterial(); // For effect
+    model(head);
+  }
   
 }
 
@@ -66,6 +87,15 @@ let getAPI = async () => {
     
 };
 
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// async function timeSensativeAction(){ //must be async func
+//   //do something here
+//   await sleep(5000) //wait 5 seconds
+//   //continue on...
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
     // document.documentElement.webkitRequestFullScreen();
@@ -82,38 +112,72 @@ document.addEventListener('DOMContentLoaded', function() {
     setInterval(function(){ 
       var joy_X = joy.GetX(); 
       var joy_Y = joy.GetY();
-      if(joy_X != prevX1 || joy_Y != prevY1) {
-        console.log("Joy1 = X " + joy_X + " | Y " + joy_Y)
-      }
+      if(inverted == false){
+        if(joy_X != prevX1 || joy_Y != prevY1) {
+          console.log("Joy1 = X " + joy_X + " | Y " + joy_Y)
+        }
 
-      if (joy_Y < 0 && position_x > -0.25){
-        position_x += joy_Y * 0.0005;
-      }
-      if (joy_Y > 0 && position_x < 0.35){
-        position_x += joy_Y * 0.0005;
-      }
-      
-      if (joy_X < 0 && position_y > -0.25){
-        position_y += joy_X * 0.0005;
-      }
-      if (joy_X > 0 && position_y < 0.25){
-        position_y += joy_X * 0.0005;
-      }
-      redraw()
+        if (joy_Y < 0 && position_x > -0.25){
+          position_x += joy_Y * 0.0005;
+        }
+        if (joy_Y > 0 && position_x < 0.35){
+          position_x += joy_Y * 0.0005;
+        }
+        
+        if (joy_X < 0 && position_y > -0.25){
+          position_y += joy_X * 0.0005;
+        }
+        if (joy_X > 0 && position_y < 0.25){
+          position_y += joy_X * 0.0005;
+        }
+        redraw()
 
-      var joy2_X = joy2.GetX(); 
-      var joy2_Y = joy2.GetY();
-      if(joy2_X != prevX2 || joy2_Y != prevY2) {
-        console.log("Joy2 = X " + joy2_X + " | Y " + joy2_Y)
-      }
+        var joy2_X = joy2.GetX(); 
+        var joy2_Y = joy2.GetY();
+        if(joy2_X != prevX2 || joy2_Y != prevY2) {
+          console.log("Joy2 = X " + joy2_X + " | Y " + joy2_Y)
+        }
 
-      prevX1 = joy_X;
-      prevY1 = joy_Y;
-      prevX2 = joy2_X;
-      prevY2 = joy2_Y;
+        prevX1 = joy_X;
+        prevY1 = joy_Y;
+        prevX2 = joy2_X;
+        prevY2 = joy2_Y;
+      }
+      if(inverted == true){
+        if(joy_X != prevX1 || joy_Y != prevY1) {
+          console.log("Joy1 = X " + joy_X + " | Y " + joy_Y)
+        }
+
+        if (joy_Y < 0 && - position_x > -0.25){
+          position_x += - joy_Y * 0.0005;
+        }
+        if (joy_Y > 0 && - position_x < 0.35){
+          position_x += - joy_Y * 0.0005;
+        }
+        
+        if (joy_X < 0 && - position_y > -0.25){
+          position_y += - joy_X * 0.0005;
+        }
+        if (joy_X > 0 && - position_y < 0.25){
+          position_y += - joy_X * 0.0005;
+        }
+        redraw()
+
+        var joy2_X = joy2.GetX(); 
+        var joy2_Y = joy2.GetY();
+        if(joy2_X != prevX2 || joy2_Y != prevY2) {
+          console.log("Joy2 = X " + joy2_X + " | Y " + joy2_Y)
+        }
+
+        prevX1 = joy_X;
+        prevY1 = joy_Y;
+        prevX2 = joy2_X;
+        prevY2 = joy2_Y;
+      }
     }, 100);
 
     var btn_wink_left = document.querySelector(".js-wink-left")
+    var btn_wink_both = document.querySelector(".js-wink-both")
     var btn_wink_right = document.querySelector(".js-wink-right")
 
     btn_wink_left.addEventListener("click", function() {
@@ -122,29 +186,55 @@ document.addEventListener('DOMContentLoaded', function() {
       } else btn_wink_left.classList.add("active");
     }); 
     
+    btn_wink_both.addEventListener("click", function() {
+      if (btn_wink_both.classList.contains("active")) {
+        btn_wink_both.classList.remove("active");
+      } else btn_wink_both.classList.add("active");
+    }); 
+    
     btn_wink_right.addEventListener("click", function() {
       if (btn_wink_right.classList.contains("active")) {
         btn_wink_right.classList.remove("active");
       } else btn_wink_right.classList.add("active");
     }); 
 
+    var radio_voor = document.querySelector(".js-voor_aanzicht")
+    var radio_achter = document.querySelector(".js-achter_aanzicht")
 
-    // const btnfull =  document.querySelector(".js-button-full")
-    // btnfull.addEventListener("click",function(){
-    //     let elem = document.documentElement;
-    //     if (elem.requestFullscreen) {
-    //         elem.requestFullscreen();
-    //       } else if (elem.mozRequestFullScreen) { /* Firefox */
-    //         elem.mozRequestFullScreen();
-    //       } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-    //         elem.webkitRequestFullscreen();
-    //       } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    //         elem.msRequestFullscreen();
-    //       }
-        
-    //     // document.documentElement.webkitRequestFullScreen();
-    //     screen.orientation.lock('landscape');
-    // })  
+    radio_voor.addEventListener("change", function() {
+      if(radio_voor.checked == true){
+        inverted = false
+      }
+      redraw()
+    });
+    
+    radio_achter.addEventListener("change", function() {
+      if(radio_achter.checked == true){
+        inverted = true
+      }
+      redraw()
+    });
+    
+
+
+    var checkbox_settings = document.querySelector(".js-checkbox-settings")
+    var body_page = document.querySelector(".js-body")
+    checkbox_settings.addEventListener('change', function() {
+      console.log("settings")
+      if(this.checked) {
+        body_page.classList.add("settings-active")
+      } else {
+        body_page.classList.remove("settings-active")
+      }
+    });
+
+    var background_settings = document.querySelector(".js-background")
+    background_settings.addEventListener('click', function() {
+      body_page.classList.remove("settings-active")
+      checkbox_settings.checked = false;
+    });
+
+    
     
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('./serviceWorker.js')
